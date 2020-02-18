b1fae25c20074a343525d63a23148fbfaa2ebddf
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-02-18 02:50:54 +0100

    Rename the handler from :za-* to →za-* to allow using on Windows

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -4,18 +4,18 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz :za-patch-dl-handler
+autoload -Uz →za-patch-dl-handler
 
 # An empty stub to fill the help handler fields
-:za-patch-dl-help-null-handler() { :; }
+→za-patch-dl-help-null-handler() { :; }
 
 # Register !atclone hook
 @zinit-register-annex "z-a-patch-dl" hook:\!atclone \
-    :za-patch-dl-handler \
-    :za-patch-dl-help-null-handler \
+    →za-patch-dl-handler \
+    →za-patch-dl-help-null-handler \
     "dl''|patch''" # register a new ice-mod: test''
 
 # Register !atpull hook
 @zinit-register-annex "z-a-patch-dl" hook:\!atpull \
-    :za-patch-dl-handler \
-    :za-patch-dl-help-null-handler
+    →za-patch-dl-handler \
+    →za-patch-dl-help-null-handler
