34ceb46736509ffc6549d5347d2587f0df9c3d5d
Salvydas Lukosius <sall@w-ss.io>
2021-12-06 08:44:37 +0000

    modified:   z-a-patch-dl.plugin.zsh 	new file:   za-patch-dl-handler 	deleted:    "\342\206\222za-patch-dl-handler"

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -1,21 +1,15 @@
 # According to the Zsh Plugin Standard:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
+# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-autoload -Uz →za-patch-dl-handler
+autoload -Uz za-patch-dl-handler
 
 # An empty stub to fill the help handler fields
-→za-patch-dl-help-null-handler() { :; }
+za-patch-dl-help-null-handler() { :; }
 
 # Register !atclone hook
-@zi-register-annex "z-a-patch-dl" hook:\!atclone-20 \
-    →za-patch-dl-handler \
-    →za-patch-dl-help-null-handler \
-    "dl''|patch''" # register a new ice-mod: test''
+@zi-register-annex "z-a-patch-dl" hook:\!atclone-20 za-patch-dl-handler za-patch-dl-help-null-handler "dl''|patch''" # register a new ice-mod: test''
 
 # Register !atpull hook
-@zi-register-annex "z-a-patch-dl" hook:\!atpull-20 \
-    →za-patch-dl-handler \
-    →za-patch-dl-help-null-handler
+@zi-register-annex "z-a-patch-dl" hook:\!atpull-20 za-patch-dl-handler za-patch-dl-help-null-handler
