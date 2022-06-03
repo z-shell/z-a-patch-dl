1b6f56e28ac3be4384e78df7377a965b5de36943
Salvydas Lukosius <sall@w-ss.io>
2022-06-03 23:50:06 +0100

    Update z-a-patch-dl.plugin.zsh

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -19,14 +19,14 @@ if [[ $PMSPEC != *f* ]] {
 autoload -Uz →za-patch-dl-handler
 
 # An empty stub to fill the help handler fields
-→za-patch-dl-help-null-handler() { :; }
+→za-patch-dl-null-handler() { :; }
 
 # Register !atclone hook
 @zi-register-annex "z-a-patch-dl" hook:\!atclone-20 \
   →za-patch-dl-handler \
-  →za-patch-dl-help-null-handler "dl''|patch''" # register a new ice-mod: dl'', patch''
+  →za-patch-dl-null-handler "dl''|patch''" # register a new ice-mod: dl'', patch''
 
 # Register !atpull hook
 @zi-register-annex "z-a-patch-dl" hook:\!atpull-20 \
   →za-patch-dl-handler \
-  →za-patch-dl-help-null-handler
+  →za-patch-dl-null-handler
