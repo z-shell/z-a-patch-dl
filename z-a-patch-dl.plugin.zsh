88482b28151077b6c714ac6dc632b38d3c09390d
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:49:01 +0000

    Update z-a-patch-dl.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- z-a-patch-dl.plugin.zsh
+++ z-a-patch-dl.plugin.zsh
@@ -6,16 +6,16 @@
 # vim: ft=zsh sw=2 ts=2 et
 #
 # According to the Zsh Plugin Standard:
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#funtions-directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#funtions-directory
 if [[ $PMSPEC != *f* ]] {
     fpath+=( "${0:h}/functions" )
 }
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
+# https://wiki.zshell.dev/community/zsh_plugin_standard/#the-proposed-function-name-prefixes
 autoload -Uz .z-a-patch-dl-download-file-stdout →za-patch-dl-handler
 
 # An empty stub to fill the help handler fields
